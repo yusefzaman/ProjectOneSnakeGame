bb8ae34640f2fcdabe53d98859743ee0ab36fb3b
yusefzaman <yusef96@gmail.com>
2024-05-07 23:19:01 +0300

    created and displayed snake head on grid

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const board = document.getElementById(`arena`)
 
-let snake = [{x: 12, y:12}];
+let snake = [{ x: 5, y: 5 }]
 
 function construct() {
-  board.innerHTML = null;
-  constructSnake();
+  board.innerHTML = ''
+  constructSnake()
 }
 
+function constructSnake() {
+  snake.forEach((segment) => {
+    const snakeUnit = createSnakeUnit('div', 'snake')
+    initiatePos(snakeUnit, segment)
+    board.appendChild(snakeUnit)
+  })
+}
 
+function createSnakeUnit(indentifier, className) {
+  const element = document.createElement(indentifier)
+  element.className = className
+  return element
+}
 
+function initiatePos(element, position) {
+  element.style.gridColumn = position.x
+  element.style.gridRow = position.y
+}
+
+constructSnake()
