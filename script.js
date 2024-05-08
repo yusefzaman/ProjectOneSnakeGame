0944f88d3dea7028366dadfe2f130de98b90ded7
yusefzaman <yusef96@gmail.com>
2024-05-08 12:31:11 +0300

    created function to generate apple and place on random unit on board

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
 const board = document.getElementById(`arena`)
 
-let snake = [{ x: 5, y: 5 }]
+const gridSize = 20
+let snake = [{ horizontal: 5, vertical: 5 }]
+let apple = generateApple()
 
 function construct() {
   board.innerHTML = ''
   constructSnake()
+  placeApple()
 }
 
 function constructSnake() {
   snake.forEach((segment) => {
-    const snakeUnit = createSnakeUnit('div', 'snake')
+    const snakeUnit = createUnit('div', 'snake')
     initiatePos(snakeUnit, segment)
     board.appendChild(snakeUnit)
   })
 }
 
-function createSnakeUnit(indentifier, className) {
+function createUnit(indentifier, className) {
   const element = document.createElement(indentifier)
   element.className = className
   return element
 }
 
 function initiatePos(element, position) {
-  element.style.gridColumn = position.x
-  element.style.gridRow = position.y
+  element.style.gridColumn = position.horizontal
+  element.style.gridRow = position.vertical
 }
 
-constructSnake()
+function generateApple() {
+  const horizontal = Math.floor(Math.random() * gridSize) + 1;
+  const vertical = Math.floor(Math.random() * gridSize) + 1;
+  return {horizontal, vertical};
+}
+
+function placeApple() {
+  const appleUnit = createUnit('div', 'apple')
+  initiatePos(appleUnit, apple)
+  board.appendChild(appleUnit)
+}
